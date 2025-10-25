cfa1f94deb5638768dda68311af162d366e006af
VO BAO TRAN <tranvo.developer@gmail.com>
2025-10-25 12:29:09 +0000

    feat: integrate highlight directive into calculator component

--- src/app/features/calculator/calculator.spec.ts
+++ src/app/features/calculator/calculator.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from '../../core/services/calculator-service';
 import { Calculator } from './calculator';
+import { By } from '@angular/platform-browser';
+import { HighlightDirective } from '../../core/ui/highlight-directive';
 
 describe('Calculator', () => {
   let component: Calculator;
@@ -10,7 +12,7 @@ describe('Calculator', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Calculator],
+      imports: [Calculator, HighlightDirective],
       providers: [CalculatorService],
     })
     .compileComponents();
@@ -63,5 +65,79 @@ describe('Calculator', () => {
     expect(() => component.divide(6, 0)).toThrow('Cannot divide by zero');
     expect(calculationServiceSpy).toHaveBeenCalledWith(6, 0);
     expect(component.result).toEqual(Number.NaN);
+  });
+
+  it('should highlight the result in red backgound when division by zero occurs', () => {
+    try {
+      component.divide(6, 0);
+    } catch(error) {
+      fixture.detectChanges();
+      expect(component.errorHighlight).toBe('red');
+      const resultElement: HTMLElement = fixture.debugElement.query(By.css('.result')).nativeElement;
+      expect(resultElement.style.backgroundColor).toBe('red');
+    }
+  });
+
+  it('should not highlight the result when performing add operation of 2 valid numbers after dividing by zero error', () => {
+    // cause division by zero error
+    try {
+      component.divide(6, 0);
+    } catch(error) {
+      fixture.detectChanges();
+    }
+    // add two numbers now
+    component.add(2, 3);
+    fixture.detectChanges();
+    expect(component.errorHighlight).toBe('');
+    const resultElement: HTMLElement = fixture.debugElement.query(By.css('.result')).nativeElement;
+    expect(resultElement.style.backgroundColor).toBe('');
+  });
+
+  it('should not highlight the result when performing subtract operation of 2 valid numbers after dividing by zero erro', () => {
+    // cause division by zero error
+    try {
+      component.divide(6, 0);
+    } catch(error) {
+      fixture.detectChanges();
+    }
+
+    // subtract two numbers now
+    component.subtract(5, 3);
+    fixture.detectChanges();
+    expect(component.errorHighlight).toBe('');
+    const resultElementAfterSubtract: HTMLElement = fixture.debugElement.query(By.css('.result')).nativeElement;
+    expect(resultElementAfterSubtract.style.backgroundColor).toBe('');
+  });
+
+  it('should not highlight the result when performing multiply operation of 2 valid numbers after dividing by zero error', () => {
+    // cause division by zero error
+    try {
+      component.divide(6, 0);
+    } catch(error) {
+      fixture.detectChanges();
+    }
+
+    // multiply two numbers now
+    component.multiply(2, 3);
+    fixture.detectChanges();
+    expect(component.errorHighlight).toBe('');
+    const resultElementAfterMultiply: HTMLElement = fixture.debugElement.query(By.css('.result')).nativeElement;
+    expect(resultElementAfterMultiply.style.backgroundColor).toBe('');
+  });
+
+  it('should not highlight the result when performing divide operation of 2 valid numbers after dividing by zero error', () => {
+    // cause division by zero error
+    try {
+      component.divide(6, 0);
+    } catch(error) {
+      fixture.detectChanges();
+    }
+
+    // divide two valid numbers now
+    component.divide(6, 2);
+    fixture.detectChanges();
+    expect(component.errorHighlight).toBe('');
+    const resultElementAfterDivide: HTMLElement = fixture.debugElement.query(By.css('.result')).nativeElement;
+    expect(resultElementAfterDivide.style.backgroundColor).toBe('');
   });
 });
--- src/app/features/calculator/calculator.ts
+++ src/app/features/calculator/calculator.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CalculatorService } from '../../core/services/calculator-service';
+import { HighlightDirective } from '../../core/ui/highlight-directive';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [HighlightDirective],
   templateUrl: './calculator.html',
   styleUrl: './calculator.scss',
 })
 export class Calculator {
   result!: number;
+  errorHighlight: string = '';
   calculationService = inject(CalculatorService);
 
   ngOnInit() {
@@ -16,22 +18,27 @@ export class Calculator {
   }
 
   add(arg0: number, arg1: number) {
+    this.errorHighlight = '';
     this.result = this.calculationService.add(arg0, arg1);
   }
 
   subtract(arg0: number, arg1: number) {
+    this.errorHighlight = '';
     this.result = this.calculationService.subtract(arg0, arg1);
   }
 
   multiply(arg0: number, arg1: number) {
+    this.errorHighlight = '';
     this.result = this.calculationService.multiply(arg0, arg1);
   }
 
   divide(arg0: number, arg1: number) {
+    this.errorHighlight = '';
     try {
       this.result = this.calculationService.divide(arg0, arg1);
     } catch (error) {
       this.result = Number.NaN;
+      this.errorHighlight = 'red';
       throw error;
     }
   }
